860af1f92963cceba31c79e3dbb77bf028866d87
eodovo <eodovo@naver.com>
2022-11-23 18:20:38 +0900

    commit

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,6 +6,7 @@ const ExpenseForm = () => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
+
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
   };
@@ -16,22 +17,37 @@ const ExpenseForm = () => {
     setEnteredDate(event.target.value);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseDate = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseDate);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className=".new-expense__control">
           <label>Title</label>
-          <input tpye="text" onChange={titleChangeHandler} />
+          <input tpye="text" onChange={titleChangeHandler} value={enteredTitle} />
         </div>
 
         <div className=".new-expense__control">
           <label>Amount</label>
-          <input tpye="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+          <input tpye="number" min="0.01" step="0.01" onChange={amountChangeHandler} value={enteredAmount} />
         </div>
 
         <div className=".new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} value={enteredDate} />
         </div>
       </div>
       <div className="new-expense__actions">
